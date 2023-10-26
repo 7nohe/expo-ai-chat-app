5daa5d541021d4a3a7c2c89391f9e1d234312234
Daiki Urata <7nohe.urata@gmail.com>
2023-10-26 15:51:36 +0900

    feat: add react-native-vercel-ai

--- App.tsx
+++ App.tsx
@@ -1,10 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { useChat } from "react-native-vercel-ai";
 
 export default function App() {
+  const { messages, input, handleInputChange, handleSubmit, data, isLoading } =
+    useChat({
+      api: "http://localhost:3000/api/chat",
+    });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <ScrollView >
+        {messages.length > 0
+          ? messages.map((m) => (
+              <Text
+                style={{
+                  marginBottom: 10,
+                  borderWidth: 1,
+                  paddingVertical: 10,
+                  paddingHorizontal: 20,
+                }}
+                key={m.id}
+              >
+                {m.role === "user" ? "🧔 User: " : "🤖 AI: "}
+                {m.content}
+              </Text>
+            ))
+          : null}
+
+        {isLoading && Platform.OS !== "web" && (
+          <View>
+            <Text>Loading...</Text>
+          </View>
+        )}
+      </ScrollView>
+      <View style={{ height: 240, width: '100%' }}>
+        <View style={{ marginTop: 60 }}>
+          <TextInput
+            value={input}
+            placeholder="Say something..."
+            style={{
+              borderWidth: 1,
+              minHeight: 50,
+              padding: 10,
+              marginBottom: 10,
+            }}
+            onChangeText={(e) => {
+              handleInputChange(
+                Platform.OS === "web" ? { target: { value: e } } : e
+              );
+            }}
+          />
+          <View
+            style={{
+              backgroundColor: "#f2f2f2f2",
+              borderWidth: 1,
+              borderColor: "black",
+            }}
+          >
+            <Button onPress={handleSubmit} title="Send" />
+          </View>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +78,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    paddingTop: 60,
   },
 });
